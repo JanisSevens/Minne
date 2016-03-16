bdcdd14a4a962b8cf241173ce6659eb03a1252e9
Admin <admin@MacSav-2.local>
2016-03-16 14:02:15 +0100

    Navigation, rating

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,9 +1,9 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    if ($(".navbar").offset().top > 150) {
+        $(".navbar").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar").removeClass("top-nav-collapse");
     }
 });
 
